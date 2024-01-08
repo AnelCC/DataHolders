951145031103ccc00c6b8c3ece5bcc108e334ab2
AnelCC <anel.calvo.cervantes@gmail.com>
2024-01-07 20:23:53 -0500

    State Flow in the view model

--- app/src/main/java/com/anelcc/dataholders/presentation/MainViewModel.kt
+++ app/src/main/java/com/anelcc/dataholders/presentation/MainViewModel.kt
@@ -3,13 +3,24 @@ package com.anelcc.dataholders.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainViewModel : ViewModel() {
-    private val _liveData = MutableLiveData<String>("Hello World!")
+    //Live data contain data with even rotate to phone
+    private val _liveData = MutableLiveData("Hello World!")
     val liveData: LiveData<String> = _liveData
+
+    //State flow use to store state and the flow benefit to update the UI
+    //this is part of the curutine framework to collect data.
+    private val _stateFlow = MutableStateFlow("Hello World!")
+    val stateFlow: MutableStateFlow<String> = _stateFlow
 
 
     fun triggerLiveData() {
         _liveData.value = "Live Data Triggered!"
+    }
+
+    fun triggerStateFlow() {
+        _liveData.value = "State Flow Triggered!"
     }
 }
