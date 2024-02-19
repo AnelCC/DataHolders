bb0aa4064164ddf96b5180c5a79f0300e497ccb2
AnelCC <anel.calvo.cervantes@gmail.com>
2024-02-19 00:06:50 -0500

    Add share Flow to the UI

--- app/src/main/java/com/anelcc/dataholders/presentation/MainViewModel.kt
+++ app/src/main/java/com/anelcc/dataholders/presentation/MainViewModel.kt
@@ -3,15 +3,13 @@ package com.anelcc.dataholders.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flow
 
 class MainViewModel : ViewModel() {
     //Live data contain data with even rotate to phone
@@ -23,14 +21,15 @@ class MainViewModel : ViewModel() {
     //this is part of the coroutine framework to collect data.
     //Flows can be a hot flow (they will collecting values)
     // and cold flow will if there is not collector
+    //this is most an one time event to collect the value and keep it and can be trigger with the lifecycle when phone rotate
     private val _stateFlow = MutableStateFlow("Hello World!")
     val stateFlow = _stateFlow.asStateFlow()
 
     //Share flow will keep the value and be able to access the data after any time on the state
     // while state flow will keep the data but only recover if we rotate the phone
+    //this is a hot flow and can be a shared and can not be trigger with the lifecycle when phone rotate
     private val _shareFlow = MutableSharedFlow<String>()
     val shareFlow = _shareFlow.asSharedFlow()
-
 
     fun triggerLiveData() {
         _liveData.value = "Live Data Triggered!"
